2c888e82f05ec3dc1119354bac15169602d0732a
PolyChromaticLithic <PolyChromaticLithic@outlook.jp>
2025-01-14 21:33:30 +0900

    リセットCSSの追加、フッターのコンパクト化

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -7,14 +7,14 @@ function Footer() {
     return (
         <nav className={styles.footer}>
             <div className={styles.footerText}>
+                Astar / PolyChromaticLithic
                 <ul className={styles.footerLinks}>
-                    <li>Astar / PolyChromaticLithic</li>
                     <li>Twitter : <a href="https://x.com/PolyChromaticLi">@PolyChromaticLi</a></li>
                     <li>Misskey : <a href="https://misskey.io/@As6">@As6</a></li>
                     <li>GitHub : <a href="https://github.com/PolyChromaticLithic">PolyChromaticLithic</a></li>
                     <li>Mail : <a href="mailto:PolyChromaticLithic@outlook.jp">PolyChromaticLithic@outlook.jp</a></li>
-                    <li>Copyright © 2024 Astar(PolyChromaticLithic). All rights reserved.</li>
                 </ul>
+                Copyright © 2024 Astar(PolyChromaticLithic). All rights reserved.
             </div>
         </nav>
     );
