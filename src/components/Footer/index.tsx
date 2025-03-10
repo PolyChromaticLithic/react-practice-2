738b5dd44a65c8e0cf7318288292c5926a6310ba
PolyChromaticLithic <PolyChromaticLithic@outlook.jp>
2025-03-10 15:24:30 +0900

    フッターのスタイルと構造を微調整

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -14,7 +14,7 @@ function Footer() {
                     <li>GitHub : <a href="https://github.com/PolyChromaticLithic">PolyChromaticLithic</a></li>
                     <li>Mail : <a href="mailto:PolyChromaticLithic@outlook.jp">PolyChromaticLithic@outlook.jp</a></li>
                 </ul>
-                Copyright © 2024 Astar(PolyChromaticLithic).<br className={styles.brMobileOnly} /> All rights reserved.
+                <p className={styles.copyright}>Copyright © 2024 Astar(PolyChromaticLithic).<br className={styles.brMobileOnly} /> All rights reserved.</p>
             </div>
         </nav>
     );
